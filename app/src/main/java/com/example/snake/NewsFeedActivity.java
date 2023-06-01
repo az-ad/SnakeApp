c0904ad2cd6293c902e052986f4b1eadea5a3451
az-ad <tasmim.cu.cse567@gmail.com>
2023-06-01 15:41:02 +0600

    post done

--- app/src/main/java/com/example/snake/NewsFeedActivity.java
+++ app/src/main/java/com/example/snake/NewsFeedActivity.java
@@ -1,19 +1,28 @@
 package com.example.snake;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,7 +31,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,11 +48,16 @@ public class NewsFeedActivity extends AppCompatActivity implements NavigationVie
     Toolbar toolbar;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
-    DatabaseReference mUserRef;
+    DatabaseReference mUserRef,postRef;
+    StorageReference  postImageRef;
     String profileimageUrlv,nameV,emailV;
     CircleImageView profileImageHeader;
     TextView nameHeader,emailHeader;
-
+    ImageView addImagePost,sendImagePost;
+    EditText inputPostDesc;
+    Uri imageUri;
+    ProgressDialog mLoadingBar;
+    private static final int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +67,10 @@ public class NewsFeedActivity extends AppCompatActivity implements NavigationVie
         drawerLayout=findViewById(R.id.drawer_layout);
         navigationView=findViewById(R.id.nav_view);
         toolbar=findViewById(R.id.toolbar1);
-
+        addImagePost=findViewById(R.id.addImagePost_Id);
+        sendImagePost=findViewById(R.id.sendPostImageView_Id);
+        inputPostDesc=findViewById(R.id.inputAddPost_Id);
+        mLoadingBar=new ProgressDialog(this);
 //        /*---------------------Toolbar-------------------------*/
 //
 //        //setSupportActionBar(toolbar);
@@ -71,7 +95,95 @@ public class NewsFeedActivity extends AppCompatActivity implements NavigationVie
         mAuth=FirebaseAuth.getInstance();
         mUser=mAuth.getCurrentUser();
         mUserRef= FirebaseDatabase.getInstance().getReference().child("Users");
+        postRef= FirebaseDatabase.getInstance().getReference().child("Posts");
+        postImageRef= FirebaseStorage.getInstance().getReference().child("PostImages");
+        //-------------------send button--------------------------------------//
+        sendImagePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddPost();
+            }
+        });
+        //------------open file chooser & select image-----------//
+        addImagePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent,REQUEST_CODE);
+            }
+        });
     } // end oncreate
+    //---------------for select image------------//
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_CODE && resultCode==RESULT_OK && data!=null){
+            imageUri =data.getData();
+            addImagePost.setImageURI(imageUri);
+
+        }
+    }
+    private void AddPost() {
+        String postDesc=inputPostDesc.getText().toString();
+        if(postDesc.isEmpty() || postDesc.length()<3)
+        {
+            inputPostDesc.setError("Please write something in Post Description");
+        }
+        else if(imageUri==null){
+            Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            mLoadingBar.setTitle("Adding Post");
+            mLoadingBar.setCanceledOnTouchOutside(false);
+            mLoadingBar.show();
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+            String strDate = formatter.format(date);
+            //-------------store image to database----------------//
+            postImageRef.child(mUser.getUid()+strDate).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful())
+                    {
+                        //---------get the url of stored image & store it to database----//
+                        postImageRef.child(mUser.getUid()+strDate).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+
+                                HashMap hashMap=new HashMap();
+                                hashMap.put("datePost",strDate);
+                                hashMap.put("postImageUrl",uri.toString());
+                                hashMap.put("postDesc",postDesc);
+                                hashMap.put("userProfileImageUrl",profileimageUrlv);
+                                hashMap.put("name",nameV);
+                                postRef.child(mUser.getUid()+strDate).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                                    @Override
+                                    public void onComplete(@NonNull Task task) {
+                                        if(task.isSuccessful()){
+                                            mLoadingBar.dismiss();
+                                            Toast.makeText(NewsFeedActivity.this, "Post Added", Toast.LENGTH_SHORT).show();
+                                            addImagePost.setImageResource(R.drawable.ic_add_post_image);
+                                            inputPostDesc.setText("");
+                                        }
+                                        else {
+                                            mLoadingBar.dismiss();
+                                            Toast.makeText(NewsFeedActivity.this, ""+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        mLoadingBar.dismiss();
+                        Toast.makeText(NewsFeedActivity.this, ""+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+    }
 
     @Override
     protected void onStart() {
